9f0a89440f9b24d205e50e457700182ecd48f765
André Biseth <andre@biseth.net>
2021-12-13 13:55:46 +0100

    More objects available through export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,28 @@
 import { ComfortCloud } from './ComfortCloud';
-export { ComfortCloud };
+import { UpdateResponse } from './models/UpdateResponse';
+import { LoginRequest } from './models/LoginRequest';
+import { LoginResponse } from './models/LoginResponse';
+import { Device, DeviceParameters, Group, GroupDevice, GroupResponse, IaqStatus, ModeAvlList } from './models/interfaces';
+import { AirSwingLR, AirSwingUD, EcoMode, FanAutoMode, FanSpeed, NanoeMode, OperationMode, Power, dataMode } from './models/enums';
+export {
+    ComfortCloud,
+    Device,
+    DeviceParameters,
+    Group,
+    GroupDevice,
+    GroupResponse,
+    IaqStatus,
+    ModeAvlList,
+    AirSwingLR,
+    AirSwingUD,
+    EcoMode,
+    FanAutoMode,
+    FanSpeed,
+    NanoeMode,
+    OperationMode,
+    Power,
+    dataMode,
+    UpdateResponse,
+    LoginRequest,
+    LoginResponse
+};
