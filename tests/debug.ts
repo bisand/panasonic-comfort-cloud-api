5f2d7c70b0dd59c294d15edf88fabb113eabc129
André Syvertsen Biseth <andre@biseth.net>
2023-09-05 07:21:06 +0200

    Added try catch to debug block

--- tests/debug.ts
+++ tests/debug.ts
@@ -7,40 +7,45 @@ import { EcoMode, AirSwingLR, AirSwingUD, FanAutoMode, FanSpeed, OperationMode, 
 dotenv.config();
 
 const runner = new Promise<any>(async (resolve, reject) => {
-    const client = new ComfortCloud(process.env.USERNAME as string, process.env.PASSWORD as string);
+    try {
+        const client = new ComfortCloud(process.env.USERNAME as string, process.env.PASSWORD as string);
 
-    const token = await client.login();
-    console.log(token);
+        const token = await client.login();
+        console.log(token);
 
-    const groups = await client.getGroups();
-    console.log(groups);
+        const groups = await client.getGroups();
+        console.log(groups);
 
-    const deviceId = groups[0].deviceList[0].deviceGuid;
-    const device = await client.getDevice(deviceId);
-    console.log(device);
+        const deviceId = groups[0].deviceList[0].deviceGuid;
+        const device = await client.getDevice(deviceId);
+        console.log(device);
 
-    const deviceNow = await client.getDeviceNow(deviceId);
-    console.log(deviceNow);
+        const deviceNow = await client.getDeviceNow(deviceId);
+        console.log(deviceNow);
 
-    const par: DeviceParameters = {
-        operate: Power.On,
-        operationMode: OperationMode.Auto,
-        ecoMode: EcoMode.Auto,
-        temperatureSet: 22,
-        airSwingUD: AirSwingUD.Mid,
-        airSwingLR: AirSwingLR.Mid,
-        fanAutoMode: FanAutoMode.AirSwingAuto,
-        fanSpeed: FanSpeed.Auto,
-    };
-    const parRes = await client.setParameters(deviceId, par);
-    console.log(parRes);
+        const par: DeviceParameters = {
+            operate: Power.On,
+            operationMode: OperationMode.Auto,
+            ecoMode: EcoMode.Auto,
+            temperatureSet: 22,
+            airSwingUD: AirSwingUD.Mid,
+            airSwingLR: AirSwingLR.Mid,
+            fanAutoMode: FanAutoMode.AirSwingAuto,
+            fanSpeed: FanSpeed.Auto,
+        };
+        const parRes = await client.setParameters(deviceId, par);
+        console.log(parRes);
 
-    device.parameters.temperatureSet = 22;
-    device.parameters.ecoMode = EcoMode.Quiet;
-    const parDev = await client.setDevice(device);
-    console.log(parDev);
+        device.parameters.temperatureSet = 22;
+        device.parameters.ecoMode = EcoMode.Quiet;
+        const parDev = await client.setDevice(device);
+        console.log(parDev);
 
-    resolve("OK");
+        resolve("OK");
+    } catch (error) {
+        console.error(error);
+        reject(error);
+    }
 });
 
 runner
